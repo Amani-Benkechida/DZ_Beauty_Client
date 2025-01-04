c58cf7090a6da8136298584231a6ac5a3fbf074d
hadjerbk <162328559+hadjerbk@users.noreply.github.com>
2025-01-04 19:44:42 +0100

    Update hello.js

--- front/src/homepage/hello.js
+++ front/src/homepage/hello.js
@@ -1,20 +1,28 @@
-import React from 'react'
-import image from "./image.png"
-import but from "./ourservicesgroupe.png"
+import React from 'react';
+import image from "./image.png";
+import but from "./ourservicesgroupe.png";
 
 export const Hello = () => {
   return (
-    <div className='w-full relative font-poppins '>
-        <img src={image} className='w-full'></img>
-        <button className='w-[244px] h-[67px] absolute bottom-[150px] left-[450px] bg-buttonColor text-white font-medium text-[22px]  rounded'>Book Now</button>
+    <div className="w-full relative font-poppins">
+      <img src={image} className="w-full" alt="Background" />
+      
+      {/* Centered container for buttons, moved slightly lower */}
+      <div className="absolute top-[85%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex gap-6">
         
-      <button className='w-[244px] h-[67px] absolute bottom-[150px] left-[740px]   hover:text-white rounded'>
-          <div className='  flex items-center gap-4 '><img className='h-[48px] w-[44.9px] ' src={but}></img>
-           <p className='font-[500] w-[126px] h-[30px] text-[20px] '>Our Services</p>
-          </div>
+        {/* Book Now button */}
+        <button className="w-[244px] h-[67px] bg-buttonColor text-white font-medium text-[22px] rounded hover:bg-black">
+          Book Now
         </button>
+
+        {/* Our Services button */}
+        <button className="w-[244px] h-[67px] hover:text-button_hover rounded flex items-center gap-4">
+          <img className="h-[48px] w-[44.9px]" src={but} alt="Our Services" />
+          <p className="font-[500] w-[126px] h-[30px] text-[20px]">Our Services</p>
+        </button>
+      </div>
     </div>
-    
-  )
-}
+  );
+};
+
 export default Hello;
